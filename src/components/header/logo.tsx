3d03f668b34b4d992f120ed680be3e03362c0e88
lw1725908379 <143780602+lw1725908379@users.noreply.github.com>
2024-05-22 16:51:47 +0800

    Update logo.tsx

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -4,9 +4,7 @@ export const Logo = () => {
   return (
     <h1 className='min-w-40 bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent dark:from-surface-4 dark:to-white/10 max-md:hidden max-sm:block max-sm:text-center'>
       <Link href='/'>
-        <strong className='text-2xl font-bold tracking-tighter xl:text-3xl'>
-          WENLIU
-        </strong>
+        <strong className='text-2xl font-bold tracking-tighter xl:text-3xl'>WL</strong>
         <small className='font-semibold tracking-normal xl:text-xl'>.vercel</small>
       </Link>
     </h1>
